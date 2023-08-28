22d881001350ff59780c4b67bd3c31655365f4cc
Kateattardo <youremail@domain.com>
2023-08-28 15:23:50 -0400

    utils

---
+++ utils/connection.js
@@ -0,0 +1,32 @@
+////////////////////////////////////////
+/////dependencies
+///////////////////////////////////////
+
+
+require('dotenv').config
+const mongoose = require('mongoose')
+
+////////////////////////////////////////
+/////DB connection
+///////////////////////////////////////
+const DATABASE_URL= process.env.DATABASE_URL
+
+//DB confiq object
+cont CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+
+//establsih our connection to the DB
+mongoose.connect(DATABASE_URL, CONFIG)
+
+//we tell mongoose what to do upon cetrain events
+mongoose.connection
+.on('open', () => console.log('mongoose connected'))
+.on('close', () => console.log('mongoose disconnected'))
+.on('error', (err) => console.log('mongoose error', err))
+
+////////////////////////////////////////
+/////export connection
+///////////////////////////////////////
