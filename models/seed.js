3c3813016301f44df51aafd95499eb21bc594297
Kateattardo <youremail@domain.com>
2023-08-29 11:58:08 -0400

    models/seeds.js

---
+++ models/seed.js
@@ -0,0 +1,26 @@
+//////////////////////////////////////////
+//////////Import Dependecies & Model
+/////////////////////////////////////
+const mongoose = require('../utils/connection')
+const Park = require('./park')
+
+//////////////////////////////////////////
+//////////Seed Script Code
+/////////////////////////////////////
+
+// first we gab our db variable
+const db =mongoose.connection
+
+
+//bc this is a script, we open then eventually close
+//our one time connection to the db
+db.on('open', () => {
+   //array of parks
+   const parks = [
+    {name: 'Central Park', location: 'Manhattan', walkingTrails: true, dogRun: true, bbqPicnic: true, playground: true},
+    {name: 'Prospect Park', location: 'Brooklyn', walkingTrails: true, dogRun: false, bbqPicnic: true, playground: true},
+    {name: 'Phelam Bay Park', location: 'Bronx', walkingTrails: true, dogRun: true, bbqPicnic: true, playground: true},
+    {name: 'Flushing Meadows Corona Park',location: 'Queens', walkingTrails: true, dogRun: true, bbqPicnic: true, playground: true},
+    {name: 'Freshkills Park', location:'Staten Island', walkingTrails: true, dogRun: false, bbqPicnic: true, playground: false }  
+   ]
+})
